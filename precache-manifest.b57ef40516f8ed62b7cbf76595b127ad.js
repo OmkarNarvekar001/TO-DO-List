9b31e60c56430bee031d19f7f91373b404a0fa37
OmkarNarvekar001 <onarvekar01@gmail.com>
2022-07-09 08:31:17 +0530

    Updates

---
+++ precache-manifest.b57ef40516f8ed62b7cbf76595b127ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93ebfc85d92d2cc0b16bc1a13077daf",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "ab93d5ab243f561ca8ff",
+    "url": "/To-Do-List/static/css/main.dea3b9db.chunk.css"
+  },
+  {
+    "revision": "721a1dbb86d98b225ff9",
+    "url": "/To-Do-List/static/js/2.3ba845c5.chunk.js"
+  },
+  {
+    "revision": "ab93d5ab243f561ca8ff",
+    "url": "/To-Do-List/static/js/main.b696a088.chunk.js"
+  },
+  {
+    "revision": "1e5b64d60343e6eba72b",
+    "url": "/To-Do-List/static/js/runtime~main.2fdc73a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/To-Do-List/static/media/logo.5d5d9eef.svg"
+  }
+]);
