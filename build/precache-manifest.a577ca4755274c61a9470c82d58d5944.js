631bffe81792418f334065dd7f7beb012a0acca1
OmkarNarvekar001 <onarvekar01@gmail.com>
2022-07-09 08:29:16 +0530

    deployment

---
+++ build/precache-manifest.a577ca4755274c61a9470c82d58d5944.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d6c1660a47a234a0b1e7e727f1f406",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "5e9d060f2e157a71aa20",
+    "url": "/To-Do-List/static/css/main.bbaaf43e.chunk.css"
+  },
+  {
+    "revision": "37d693c0db66880c19a2",
+    "url": "/To-Do-List/static/js/2.b1a81836.chunk.js"
+  },
+  {
+    "revision": "5e9d060f2e157a71aa20",
+    "url": "/To-Do-List/static/js/main.7888e103.chunk.js"
+  },
+  {
+    "revision": "1e5b64d60343e6eba72b",
+    "url": "/To-Do-List/static/js/runtime~main.2fdc73a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/To-Do-List/static/media/logo.5d5d9eef.svg"
+  }
+]);
